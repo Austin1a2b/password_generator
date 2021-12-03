deff979f99c7c3344d5a9d03c4ab3897a5337a65
Austin1a2b <cavalier283@gmail.com>
2021-12-03 16:26:32 +0800

    1.generate_password  功能完成 2. 在 app.js 中設定完成 generate_password 相關功能 3.新增  index 頁面 :  會顯示 generate_password 產生的結果

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const generatePassword = require('./generate_password')
 
 const port = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-
-
 app.get('/', (req, res) => {
   res.render('index')
 })
 
+//
 app.use(express.urlencoded({ extended: true }))
 
 // 用POST方式　取得 表單值 res.body
 app.post('/', (req, res) => {
-  console.log('req.body', req.body)
-  res.render('index')
+  const password = generatePassword(req.body)
+  res.render('index', { password: password })
 })
-
 
 app.listen(port, () => {
   console.log(`Express app listening on http://localhost:${port}/`)
 })
-
--- generate_password.js
+++ generate_password.js
@@ -1,10 +1,53 @@
-function generate_password() {
-  console.log(`test`)
-
+function sample(array) {
+  const index = Math.floor(Math.random() * array.length)
+  return array[index]
 }
 
+function generatePassword(options) {
+  // define things user might want
+  // 設定 全部參數
+  const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+  const upperCaseLetters = lowerCaseLetters.toUpperCase()
+  const numbers = '1234567890'
+  const symbols = '`~!@$%^&*()-_+={}[]|;:"<>,.?/'
 
+  // create a collection to store things user picked up
+  //使用者選擇要的參數
 
+  let collection = []
 
+  if (options.lowercase === 'on') {
+    collection = collection.concat(lowerCaseLetters.split(''))
+  }
 
-generate_password()
+  if (options.uppercase === 'on') {
+    collection = collection.concat(upperCaseLetters.split(''))
+  }
+
+  if (options.numbers === 'on') {
+    collection = collection.concat(numbers.split(''))
+  }
+
+  if (options.symbols === 'on') {
+    collection = collection.concat(symbols.split(''))
+  }
+
+  // remove things user do not need
+  //將 不要的字串 從清單中  移除 
+
+  if (options.excludeCharacters) {
+    collection = collection.filter((character) => !options.excludeCharacters.includes(character))
+  }
+
+  // start generating password
+  //隨機抽取字串
+  let password = ''
+  for (let i = 1; i <= Number(options.length); i++) {
+    password += sample(collection)
+  }
+  // return the generated password
+  return password
+}
+
+// export generatePassword function for other files to use
+module.exports = generatePassword
