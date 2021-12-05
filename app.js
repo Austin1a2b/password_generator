51b841ab7ca8227fe9e1303dca1ef3b97dac679c
Austin1a2b <cavalier283@gmail.com>
2021-12-05 18:52:25 +0800

    Revert "建立基本檔案"
    This reverts commit cf05f65a918717b9649f8ffbe90429f0eb0dcc1c.

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express')
-const exphbs = require('express-handlebars')
-
 const app = express()
+const exphbs = require('express-handlebars')
+const generatePassword = require('./generate_password')
+
 const port = 3000
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
-
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
   res.render('index')
 })
 
-app.listen(port, () => { console.log('localhost:3000') })
+//載入 body-parser
+app.use(express.urlencoded({ extended: true }))
+
+// 用POST方式　取得 表單值 res.body
+app.post('/', (req, res) => {
+  const password = generatePassword(req.body)
+  const options = req.body
+  res.render('index', { password: password, options: options })
+})
+
+app.listen(port, () => {
+  console.log(`Express app listening on http://localhost:${port}/`)
+})
