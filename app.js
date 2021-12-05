cf05f65a918717b9649f8ffbe90429f0eb0dcc1c
Austin1a2b <cavalier283@gmail.com>
2021-12-05 18:51:34 +0800

    建立基本檔案

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
 const express = require('express')
-const app = express()
 const exphbs = require('express-handlebars')
-const generatePassword = require('./generate_password')
 
+const app = express()
 const port = 3000
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 
 app.get('/', (req, res) => {
   res.render('index')
 })
 
-//載入 body-parser
-app.use(express.urlencoded({ extended: true }))
-
-// 用POST方式　取得 表單值 res.body
-app.post('/', (req, res) => {
-  const password = generatePassword(req.body)
-  const options = req.body
-  res.render('index', { password: password, options: options })
-})
-
-app.listen(port, () => {
-  console.log(`Express app listening on http://localhost:${port}/`)
-})
+app.listen(port, () => { console.log('localhost:3000') })
