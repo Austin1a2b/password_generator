f8af7e87db6409786a05c2acebea445bfad31a98
Austin1a2b <cavalier283@gmail.com>
2021-12-05 19:17:13 +0800

    還原 錯誤的編輯

--- app.js
+++ app.js
@@ -12,13 +12,14 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
-//
+//載入 body-parser
 app.use(express.urlencoded({ extended: true }))
 
 // 用POST方式　取得 表單值 res.body
 app.post('/', (req, res) => {
   const password = generatePassword(req.body)
-  res.render('index', { password: password })
+  const options = req.body
+  res.render('index', { password: password, options: options })
 })
 
 app.listen(port, () => {
